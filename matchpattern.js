99476598f67b03ce8a758adc34e7cf514c9a706d
jc3213 <jc3213@users.noreply.github.com>
2024-09-20 18:24:27 +0800

    Update matchpattern.js

--- matchpattern.js
+++ matchpattern.js
@@ -1,29 +1,77 @@
 class MatchPattern {
-    constructor (args) {
-        this.list = Array.isArray(args) ? args : [];
-        this.rule = {};
-        this.list.forEach((rule) => this.rule[rule] = true);
-        this.make();
+    version: '0.1';
+    cache = {};
+    list = [];
+    result = {};
+    matchpattern = /!/;
+    tlds = {
+        'aero': true,
+        'app': true,
+        'arpa': true,
+        'asia': true,
+        'biz': true,
+        'cat': true,
+        'co': true,
+        'com': true,
+        'coop': true,
+        'dev': true,
+        'edu': true,
+        'eu': true,
+        'gov': true,
+        'info': true,
+        'int': true,
+        'io': true,
+        'jobs': true,
+        'ltd': true,
+        'ltda': true,
+        'mil': true,
+        'mobi': true,
+        'museum': true,
+        'name': true,
+        'ne': true,
+        'net': true,
+        'org': true,
+        'post': true,
+        'pro': true,
+        'si': true,
+        'tel': true,
+        'test': true,
+        'travel': true,
+        'web': true,
+        'xxx': true,
+        'xyz': true
+    };
+    make (host) {
+        if (/((25[0-5]|(2[0-4]|1[0-9]|[1-9]?)[0-9])\.){3}(25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])/.test(host)) { return host.replace(/\d+\.\d+$/, '*'); }
+        let [full, sbd, sld, tld] = host.match(/(?:([^\.]+)\.)?([^\.]+)\.([^\.]+)$/);
+        if (!sbd || !this.tlds[sld]) { return '*.' + sld + '.' + tld; }
+        return '*.' + sbd + '.' + sld + '.' + tld;
     }
-    add (rule) {
-        if (this.rule[rule]) { return; }
-        this.rule[rule] = true;
-        this.list.push(rule);
-        this.make();
+    add (...hosts) {
+        hosts.flat().forEach((host) => {
+            if (this.cache[host]) { return; }
+            let rule = this.make(host);
+            this.cache[host] = rule;
+            this.list.push(rule);
+            console.log(rule);
+        });
+        this.regexp();
     }
-    remove (rule) {
-        if (!this.rule[rule]) { return; }
-        this.list.splice(this.list.indexOf(rule), 1);
-        delete this.rule[rule];
-        this.make();
+    remove (...hosts) {
+        hosts.flat().forEach((host) => {
+            let rule = this.cache[host];
+            if (!rule) { return; }
+            this.list.splice(this.list.indexOf(rule), 1);
+            delete this.cache[host];
+        });
+        this.regexp();
     }
-    make () {
-        this.regexp = this.list.length === 0 ? /!/ : new RegExp('^(' + this.list.join('|').replace(/\./g, '\\.').replace(/\\?\.?\*\\?\.?/g, '.*') + ')$');
+    regexp () {
+        let trim = [...new Set(this.list)];
+        this.matchpattern = this.list.length === 0 ? /!/ : new RegExp('^(' + trim.join('|').replace(/\./g, '\\.').replace(/\\?\.?\*\\?\.?/g, '.*') + ')$');
         this.result = {};
     }
-    match (url) {
-        let host = new URL(url).hostname;
-        if (host in this.result) { return this.result[host]; }
-        return this.result[host] = this.regexp.test(host);
+    match (host) {
+        return this.result[host] ??= this.matchpattern.test(host);
     }
 }

fc8a769bba2f0f571648b69fc4c0ef13ebdce248
jc3213 <jc3213@users.noreply.github.com>
2025-06-01 19:32:55 +0800

    Update matchpattern_0.6.js

--- legacy/matchpattern_0.6.js
+++ legacy/matchpattern_0.6.js
@@ -111,4 +111,4 @@ class MatchPattern {
         let pac_script = 'function FindProxyForURL(url, host) {' + pac.join('') + '\n    return "DIRECT";\n}';
         return { regexp , pac_script };
     }
-};
+}

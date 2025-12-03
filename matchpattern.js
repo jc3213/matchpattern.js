2c469123b239a1dc07529b4c898d2c7f937edcdd
jc3213 <jc3213@users.noreply.github.com>
2025-12-03 20:59:52 +0800

    Update matchpattern.js

--- matchpattern.js
+++ matchpattern.js
@@ -1,38 +1,61 @@
 class MatchPattern {
-    constructor (arg) {
+    #data = new Set();
+    #empty = true;
+    #global = false;
+
+    constructor(arg) {
         if (arg) {
             this.new(arg);
         }
     }
-    version = '1.0';
-    #data = new Set();
-    #dataSet = [];
-    #global = false;
-    get data () {
-        return this.#dataSet;
+
+    get data() {
+        return [...this.#data];
     }
-    new (arg) {
+
+    #update() {
+        this.#empty = this.#data.size === 0;
+        this.#global = this.#data.has('*');
+    }
+
+    new(arg) {
         this.#data = new Set(Array.isArray(arg) ? arg : [arg]);
         this.#update();
     }
-    add (arg) {
+
+    add(arg) {
         Array.isArray(arg) ? arg.forEach((i) => this.#data.add(i)) : this.#data.add(arg);
         this.#update();
     }
-    delete (arg) {
+
+    delete(arg) {
         Array.isArray(arg) ? arg.forEach((i) => this.#data.delete(i)) : this.#data.delete(arg);
         this.#update();
     }
-    clear () {
-        this.#data.clear();
-        this.#dataSet = [];
+
+    clear() {
+        this.#data = new Set();
+        this.#empty = true;
         this.#global = false;
     }
-    test (host) {
-        return this.#global || this.#data.has(host) || this.#dataSet.some((i) => host.endsWith(`.${i}`));
-    }
-    #update () {
-        this.#dataSet = [...this.#data];
-        this.#global = this.#data.has('*');
+
+    test(arg) {
+        if (this.#empty) {
+            return false;
+        }
+        if (this.#global) {
+            return true;
+        }
+        while (true) {
+            if (this.#data.has(arg)) {
+                return true;
+            }
+            let dot = arg.indexOf('.');
+            if (dot < 0) {
+                break;
+            }
+            arg = arg.substring(dot + 1);
+        }
+        return false;
     }
 }
